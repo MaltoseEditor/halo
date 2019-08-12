08ffddd7ce7c5f3f772cc1ac4a12d6a9fcc0bf88
abersheeran <me@abersheeran.com>
2019-08-12 11:41:07 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="halo" className="flex">
+      <div className="circle dark-gray outer">
+        <div className="circle light-gray inner">
+
+        </div>
+      </div>
     </div>
   );
 }
